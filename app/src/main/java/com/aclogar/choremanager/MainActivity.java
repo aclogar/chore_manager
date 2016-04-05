ae9737f989caabe976c7c8b6281f4cb0dd305af8
Anthony Logar <coledude919@gmail.com>
2016-04-05 17:45:57 -0400

    Added link from main to login

--- app/src/main/java/com/aclogar/choremanager/MainActivity.java
+++ app/src/main/java/com/aclogar/choremanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.aclogar.choremanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -82,6 +83,10 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+        } else if (id == R.id.login){
+            Intent login = new Intent(this, LoginActivity.class);
+            startActivity(login);
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
