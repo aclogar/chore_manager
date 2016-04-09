7cdfaba8418caaa5d1076875e3b71acbf2313adc
Anthony Logar <coledude919@gmail.com>
2016-04-09 14:04:27 -0400

    save an load shit using json

--- app/src/main/java/com/aclogar/choremanager/objects/User.java
+++ app/src/main/java/com/aclogar/choremanager/objects/User.java
@@ -1,27 +1,34 @@
 package com.aclogar.choremanager.objects;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by Anthony on 4/7/2016.
  */
-public class User {
+public class User implements Serializable{
+
+    private static final long serialVersionUID = 32L;
+
     private String user_id;
     private String user_name;
     private String user_email;
+    private String password;
     private ArrayList<Group> groups = new ArrayList<Group>();
 
-    public User(String user_id, String user_name, String user_email, ArrayList<Group> groups) {
+    public User(String user_id,  String password, String user_name, String user_email, ArrayList<Group> groups) {
         this.user_id = user_id;
         this.user_name = user_name;
         this.user_email = user_email;
+        this.password = password;
         this.groups = groups;
     }
 
-    public User(String user_id, String user_name, String user_email) {
+    public User(String user_id,String password,  String user_name, String user_email) {
         this.user_id = user_id;
         this.user_name = user_name;
         this.user_email = user_email;
+        this.password= password;
     }
 
     public String getUser_id() {
@@ -62,5 +69,13 @@ public class User {
 
     public boolean removeGroup(Group g){
         return groups.remove(g);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
