5cb3defe223abd5f3d1e8ef9ff864a21fc49558e
latimko <latimko@ncsu.edu>
2016-04-12 15:14:40 -0400

    Added Create Chore Page

---
+++ app/src/main/java/com/aclogar/choremanager/CreateChoreActivity.java
@@ -0,0 +1,14 @@
+package com.aclogar.choremanager;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CreateChoreActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_chore);
+    }
+
+}
